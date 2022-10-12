995502ac3357cced21c5929733175f243e0374e6
xiaoyu <xiaoyu@gini.co>
2022-10-12 23:41:41 +0800

    update version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             'absbox.local':local
         },
         packages = ['absbox','absbox.local'],
-        version = '0.1.2.1',
+        version = '0.1.2.3',
         license='Apache',
         description = 'an analytical library for cashflow modeling on ABS/MBS products',
         long_description_content_type='text/markdown',
