93345eb61b4e3946df6c18ca269dab63b83dae16
xiaoyu <always.zhang@gmail.com>
2023-01-29 12:52:33 +0800

    update version 0.8.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
             'absbox.local':local
         },
         packages = ['absbox','absbox.local'],
-        version = '0.8.0',
+        version = '0.8.1',
         license='Apache',
         description = 'an analytical library for cashflow modeling on ABS/MBS products',
         long_description_content_type='text/markdown',
