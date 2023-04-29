066b1166be491d4fc4465de9f050493e78b3d827
xiaoyu <always.zhang@gmail.com>
2023-04-30 00:12:55 +0800

    add version

--- absbox/__init__.py
+++ absbox/__init__.py
@@ -3,8 +3,14 @@ import sys,warnings
 if (sys.version_info.major >= 3 and sys.version_info.minor < 10):
     raise ImportError("AbsBox support Python with version 3.10+ only")
 
-from absbox.client import API,save,guess_pool_flow_header
+from absbox.client import API,save
+from absbox.local.util import guess_pool_flow_header
 from absbox.local.base import *
 from absbox.local.china import 信贷ABS,SPV
 from absbox.local.cmp import comp_engines
 from absbox.local.generic import Generic
+
+
+from importlib.metadata import version
+
+__version__ = version("absbox")
