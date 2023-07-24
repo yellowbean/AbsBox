8b442ba758f6b74f572ca45a8a3d08396f263384
Xiaoyu <always.zhang@gmail.com>
2023-07-25 01:35:57 +0800

    fix install

--- absbox/__init__.py
+++ absbox/__init__.py
@@ -3,7 +3,7 @@ import sys,warnings
 if (sys.version_info.major >= 3 and sys.version_info.minor < 10):
     raise ImportError("AbsBox support Python with version 3.10+ only")
 
-from absbox.client import API,save
+from absbox.client import API
 from absbox.local.util import guess_pool_flow_header
 from absbox.local.base import *
 from absbox.local.china import 信贷ABS,SPV
