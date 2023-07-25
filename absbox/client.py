7d515e7f5b437d94cdcd0f7ec6807e3d58c59458
yellowbean <always.zhang@gmail>
2023-07-25 14:16:05 +0800

    WIP: remove token if login failure

--- absbox/client.py
+++ absbox/client.py
@@ -190,6 +190,7 @@ class API:
             console.print(f"✅[bold green] login successfully,{r['msg']}")
             self.token = r['token']
         else:
+            delattr(self,'token')
             console.print(f"❌[bold red]Failed to login,{r['msg']}")
             return None
     
