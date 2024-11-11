907ca9b57a92b728b32ed4c01f9060c82102d8e8
yellowbean <always.zhang@gmail.com>
2024-11-11 20:21:37 +0800

    fix empty case

--- absbox/local/cf.py
+++ absbox/local/cf.py
@@ -132,7 +132,7 @@ def readInspect(r:dict) -> pd.DataFrame:
     else:
         u = pd.DataFrame()
 
-    w = r['waterfallInspect'].to_records() if 'waterfallInspect' in r else []
+    w = r['waterfallInspect'].to_records() if ('waterfallInspect' in r and r['waterfallInspect']) else []
     wdf = tz.pipe(tz.groupby(lambda x:tz.nth(2,x), w)
             ,lambda m: tz.valmap(lambda xs: [ (_[1],_[4]) for _ in xs],m)
             ,lambda m: tz.valmap(lambda xs: pd.DataFrame(xs, columns =['Date', 'val']).set_index('Date'),m)
