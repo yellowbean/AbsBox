8009cf0f071a318d2acfc6e353e3c974f8a2e5e2
Xiaoyu <always.zhang@gmail.com>
2025-03-08 19:58:48 +0800

    update UT

--- absbox/__init__.py
+++ absbox/__init__.py
@@ -3,7 +3,7 @@ import sys
 if (sys.version_info.major >= 3 and sys.version_info.minor < 10):
     raise ImportError("AbsBox support Python with version 3.10+ only")
 
-from absbox.client import API, Endpoints, EnginePath, PickApiFrom
+from absbox.client import API, Endpoints, EnginePath, PickApiFrom,LIBRARY
 from absbox.local.util import guess_pool_flow_header, unifyTs, mkTbl, readAeson
 from absbox.local.base import *
 from absbox.local.cmp import comp_engines
