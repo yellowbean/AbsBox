508526c543d6f08dee388bac1c3588cfcdb914db
Xiaoyu <always.zhang@gmail.com>
2025-03-16 12:30:47 +0800

    fix yield table; remove irr

--- absbox/local/analytics.py
+++ absbox/local/analytics.py
@@ -5,12 +5,27 @@ from absbox.validation import vStr
 import numpy as np
 from toolz import get_in
 
-def runYieldTable(api, d, bondName, p_assumps: dict, b_assumps: dict):
+def runYieldTable(api, d, bondName:str, p_assumps: dict, b_assumps: dict):
+    """ a shortcut function to run a 'yield table' for a bond, with different pool assumptions
+
+    :param api: the api object
+    :type api: _type_
+    :param d: the deal object
+    :type d: _type_
+    :param bondName: the bond name
+    :type bondName: str
+    :param p_assumps: a map of pool assumptions
+    :type p_assumps: dict
+    :param b_assumps: pricing assumption for bond
+    :type b_assumps: tuple
+    :return: _description_
+    :rtype: _type_
+    """
     assert isinstance(p_assumps, dict), f"pool assumption should be a map but got {type(p_assumps)}"
     
-    rs = api.run(d, poolAssump=p_assumps
-                  , runAssump=[('pricing', b_assumps)]
-                  , read=True)
+    rs = api.runByScenarios(d, poolAssump=p_assumps
+                            , runAssump=[('pricing', b_assumps)]
+                            , read=True)
 
     b_pricing = {k: v['pricing'].loc[ vStr(bondName)] for k, v in rs.items()}
     b_table = pd.concat(b_pricing.values(), axis=1)
@@ -19,26 +34,6 @@ def runYieldTable(api, d, bondName, p_assumps: dict, b_assumps: dict):
     return b_table
 
 run_yield_table = runYieldTable
-
-def irr(flow: pd.DataFrame, init):
-    def extract_cash_col(_cols):
-        if _cols == china_bondflow_fields_s:
-            return flow[china_bondflow_cash]
-        elif _cols == english_bondflow_fields_s: 
-            return flow[english_bondflow_cash]
-        else:
-            raise RuntimeError("Failed to match", _cols)
-
-    cols = flow.columns.to_list()
-    dates = flow.index.to_list()
-    amounts = extract_cash_col(cols).to_list()
-        
-    assert init is not None, f"inti:{init} shouldn't be None"
-    assert len(init) == 2, f"init: should be a tuple with length of 2, but got {init}"
-    invest_date, invest_amount = init
-    dates = [invest_date]+dates
-    amounts = [invest_amount]+amounts
-    return xirr(dates,amounts)
 
 
 def sum_fields_to_field(df: pd.DataFrame, cols: list, col: str):
@@ -63,23 +58,3 @@ def flow_by_scenario(rs, flowpath, node="col", rtn_df=True, ax=1, rnd=2):
         _ks = list(r.keys())
         r = pd.concat(_vs, keys=_ks, axis=ax)
     return r
-
-
-def viewBalanceAccount(accStmt, date=None) -> float :
-    if date is None:
-        return 1
-    
-    return 0
-
-
-
-def FlowSummary(r:dict, start_date=None, end_date=None):
-    """Sum up cash flow from result
-    """
-    accsMap = r['accounts']
-
-
-
-
-
-    return {}
